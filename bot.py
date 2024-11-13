ca37a16a3880b78e790b6d98798725f7eec2bb43
Pon4ick <krpon4ick@gmail.com>
2024-11-13 15:15:12 +0200

    ton connect v2

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ TOKEN = "7654469961:AAFYoQGShhojE9FMK8D9-l1K4q2vDKUuSgI"
 
 async def start(update: Update, context: CallbackContext):
     keyboard = [
-        [InlineKeyboardButton("Подключить кошелек", web_app=WebAppInfo(url="https://testbot-js6e916dj-vladyslavs-projects-9c4762b2.vercel.app/"))]
+        [InlineKeyboardButton("Подключить кошелек", web_app=WebAppInfo(url="https://testbot-virid-seven.vercel.app/"))]
     ]
     await update.message.reply_text("Нажмите, чтобы подключить кошелек:", reply_markup=InlineKeyboardMarkup(keyboard))
 
